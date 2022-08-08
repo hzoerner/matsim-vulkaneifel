2a536166d952b0b8e9eabd359e56d434abb78d12
hzoerner <h.zoerner@campus.tu-berlin.de>
2022-08-08 20:47:04 +0200

    script to add drt vehicles to transit vehicles file. vehicles file can be used to calculate emissions for drt vehicles

---
+++ src/main/java/Study_Analysis/AddDrtToTransitVehicles.java
@@ -0,0 +1,43 @@
+package Study_Analysis;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.vehicles.MatsimVehicleWriter;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleType;
+
+public class AddDrtToTransitVehicles {
+
+    private static final String ptVehiclesFile = "C:\\Users\\ACER\\IdeaProjects\\matsim-vulkaneifel\\output\\study\\fleet-size-60\\fleet-size-60-plan-case-1.output_transitVehicles.xml.gz";
+    private static final int fleetsize = 60;
+    private static final String runId = "fleet-size-60-plan-case-1";
+
+    public static void main(String[] args) {
+
+        Config config = ConfigUtils.createConfig();
+        config.transit().setVehiclesFile(ptVehiclesFile);
+
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+
+        var transitVehicles = scenario.getTransitVehicles();
+        var factory = transitVehicles.getFactory();
+
+        VehicleType drtType = factory.createVehicleType( Id.create("drt_veh_type", VehicleType.class ));
+        drtType.getCapacity().setSeats(1).setStandingRoom(0);
+        drtType.setPcuEquivalents(1.0);
+        drtType.setNetworkMode(TransportMode.drt);
+        transitVehicles.addVehicleType(drtType);
+
+        for(int drt_id = 0; drt_id < fleetsize; drt_id ++){
+
+            Vehicle drtVehicle = factory.createVehicle(Id.createVehicleId("drt_" + drt_id), drtType);
+            transitVehicles.addVehicle(drtVehicle);
+        }
+
+        new MatsimVehicleWriter(transitVehicles).writeFile(ptVehiclesFile);
+    }
+}
