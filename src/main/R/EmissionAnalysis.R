5b98c4886f6835a646b4c63dc81d9ec861b481d1
hzoerner <h.zoerner@campus.tu-berlin.de>
2022-08-09 14:52:54 +0200

    adjusted analysis for legs instead of trips

--- src/main/R/EmissionAnalysis.R
+++ src/main/R/EmissionAnalysis.R
@@ -3,34 +3,38 @@ library(sf)
 
 ##Emission Analysis for Base Case and both Plan Cases
 
-prepare_trips <- function(tripsFilePath, shapeFilePath){
+prepare_legs <- function(legsFilePath, shapeFilePath){
   
   
-  trips.raw = read.csv2(tripsFilePath)
+  trips.raw = read.csv2(legsFilePath)
   shape = st_read(shapeFilePath)
   
   trips.start = trips.raw %>%
     st_as_sf(coords = c("start_x", "start_y"), crs = 25832) %>%
-    st_filter(shape)
+    st_filter(shape) %>%
+    group_by(trip_id) %>%
+    mutate(leg_id = paste0(trip_id, row_number())) %>%
+    ungroup()
   
   trips.end = trips.raw %>%
     st_as_sf(coords = c("end_x", "end_y"), crs = 25832) %>%
-    st_filter(shape)
+    st_filter(shape) %>%
+    group_by(trip_id) %>%
+    mutate(leg_id = paste0(trip_id, row_number())) %>%
+    ungroup()
   
   trips.filtered = semi_join(as.data.frame(trips.start), 
-                                       as.data.frame(trips.end), by = "trip_id")
+                                       as.data.frame(trips.end), by = "leg_id")
   
   base.case.pkm = trips.filtered %>%
     
-    select(person, trip_id, longest_distance_mode, traveled_distance) %>%
-    
-    rename("mode" = "longest_distance_mode") %>%
+    select(person, leg_id, mode, distance) %>%
     
     mutate(mode = ifelse(mode == "ride", "car", mode)) %>%
     
     group_by(mode) %>%
     
-    summarise(pkm = sum(traveled_distance) / 1000) %>%
+    summarise(pkm = sum(distance) / 1000) %>%
     
     ungroup() %>%
     
@@ -42,14 +46,14 @@ prepare_trips <- function(tripsFilePath, shapeFilePath){
   base.case.pkm
 }
 
-merge_with_emissions <- function(trips, warmEmissionFilePath, coldEmissionFilePath){
+merge_with_emissions <- function(legs, warmEmissionFilePath, coldEmissionFilePath){
   
-  if(!is.data.frame(trips)) {
+  if(!is.data.frame(legs)) {
     print("TRIPS must be a data frame!!!")
     return(NULL)
   }
   
-  if("mode_ger" %in% colnames(trips) == FALSE){
+  if("mode_ger" %in% colnames(legs) == FALSE){
     
     print("TRIPS needs a col named 'mode_ger' with german translation for mode names pt = 'ÖPNV' e.g.")
   }
@@ -64,7 +68,7 @@ merge_with_emissions <- function(trips, warmEmissionFilePath, coldEmissionFilePa
     
     select(vehicleId, CO, CO2_TOTAL, PM, NOx, NO2) %>%
     
-    filter(!str_detect(vehicleId, "CustomTrain")) %>%
+#    filter(!str_detect(vehicleId, "CustomTrain")) %>%
     
     mutate(mode = ifelse(str_detect(vehicleId, "car"), "MIV", 
                          ifelse(str_detect(vehicleId, "drt"), "DRT", "ÖPNV")))
@@ -81,7 +85,7 @@ merge_with_emissions <- function(trips, warmEmissionFilePath, coldEmissionFilePa
     
     mutate_if(is.double, function(x){ x / 1000}) %>%
     
-    left_join(trips, by = c("mode" = "mode_ger")) %>%
+    left_join(legs, by = c("mode" = "mode_ger")) %>%
     
     select(- mode.y) %>%
     
@@ -94,20 +98,20 @@ SHAPEFILE = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/scenario/open-vulkane
 
 WARM_EMISSIONS_BASE_CASE = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/output/study/baseCase_warm_emissions.csv"
 COLD_EMISSIONS_BASE_CASE = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/output/study/baseCase_cold_emissions.csv"
-TRIPS_BASE_CASE = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/scenario/open-vulkaneifel-scenario/vulkaneifel-v1.0-25pct/165/165.output_trips.csv.gz"
+LEGS_BASE_CASE = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/scenario/open-vulkaneifel-scenario/vulkaneifel-v1.0-25pct/165/165.output_legs.csv.gz"
 
-base.case.pkm = prepare_trips(tripsFilePath = TRIPS_BASE_CASE, shapeFilePath = SHAPEFILE)
+base.case.pkm = prepare_legs(legsFilePath = LEGS_BASE_CASE, shapeFilePath = SHAPEFILE)
 
-base.case.sum = merge_with_emissions(trips = base.case.pkm, warmEmissionFilePath = WARM_EMISSIONS_BASE_CASE, coldEmissionFilePath = COLD_EMISSIONS_BASE_CASE)
+base.case.sum = merge_with_emissions(legs = base.case.pkm, warmEmissionFilePath = WARM_EMISSIONS_BASE_CASE, coldEmissionFilePath = COLD_EMISSIONS_BASE_CASE)
 
 ########### PLAN CASE 1 ################
 WARM_EMISSIONS_PLAN_CASE_1 = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/output/study/plan-case-1_warm_emissions.csv"
 COLD_EMISSIONS_PLAN_CASE_1 = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/output/study/plan-case-1_cold_emissions.csv"
-TRIPS_PLAN_CASE_1 = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/output/study/fleet-size-60/fleet-size-60-plan-case-1.output_trips.csv.gz"
+LEGS_PLAN_CASE_1 = "C:/Users/ACER/IdeaProjects/matsim-vulkaneifel/output/study/fleet-size-60/fleet-size-60-plan-case-1.output_legs.csv.gz"
 
-plan.case.1.pkm = prepare_trips(tripsFilePath = TRIPS_PLAN_CASE_1, shapeFilePath = SHAPEFILE)
+plan.case.1.pkm = prepare_legs(legsFilePath = LEGS_PLAN_CASE_1, shapeFilePath = SHAPEFILE)
 
-emissions.1.sum = merge_with_emissions(trips = plan.case.1.pkm, warmEmissionFilePath = WARM_EMISSIONS_PLAN_CASE_1, coldEmissionFilePath = COLD_EMISSIONS_PLAN_CASE_1)
+emissions.1.sum = merge_with_emissions(legs = plan.case.1.pkm, warmEmissionFilePath = WARM_EMISSIONS_PLAN_CASE_1, coldEmissionFilePath = COLD_EMISSIONS_PLAN_CASE_1)
 
 emissions.1.long = emissions.1.sum %>%
   
@@ -129,6 +133,8 @@ ggplot(filter(emissions.1.long, mode != "MIV"), aes(mode, emission, fill = mode)
   theme_bw() +
   
   theme(legend.position = "none")
+
+ggsave(filename = "C:/Users/ACER/Desktop/Uni/Bachelorarbeit/Grafiken/Planfall_1_Gesamtemissionen.jpg")
 
 pt.vs.drt = emissions.1.sum %>%
   
